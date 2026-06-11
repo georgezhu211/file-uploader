28947fced0c0b14dff0a1e01800edb8f6167b53f
George Zhu <georgezhu211@gmail.com>
2026-06-11 00:02:59 -0500

    feat: add password hashing

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const userRepository = require("../repositories/user.repository");
+const bcrypt = require("bcryptjs");
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -10,9 +11,13 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
+
       return done(null, user);
     } catch (err) {
       return done(err);
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,7 +1,12 @@
 const { prisma } = require("../db/prisma");
+const bcrypt = require("bcryptjs");
 
 exports.create = async ({ username, password }) => {
-  const user = await prisma.user.create({ data: { username, password } });
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: { username, password: hashedPassword },
+  });
 
   return user;
 };

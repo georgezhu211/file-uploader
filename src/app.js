735fd73c742ee226585ff4d49259292a07418426
George Zhu <georgezhu211@gmail.com>
2026-06-10 20:00:28 -0500

    feat: add signup page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 require("dotenv/config");
 
 const express = require("express");
+const path = require("node:path");
+
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => res.send("Hello, world!"));
+
+app.use("/auth", authRoutes);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {

4d5043dac72dcbb5fc8e6cbfdd01799ab45ee789
George Zhu <georgezhu211@gmail.com>
2026-06-11 10:01:38 -0500

    feat: add folder model

---
+++ prisma/migrations/20260611150048_folder/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
